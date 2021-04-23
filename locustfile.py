7acc0b2a1d3476d1c7a6efd75909abc1b3904515
vietnguyen3 <viet.nguyen3@tiki.vn>
2021-04-23 16:34:07 +0700

    update docker for monitoring

--- locustfile.py
+++ locustfile.py
@@ -3,6 +3,14 @@ from locust import HttpUser, task, between
 class QuickstartUser(HttpUser):
     wait_time = between(1, 2.5)
 
-    @task
-    def hello_world(self):
+    @task(3)
+    def fetch_product_pb(self):
+        self.client.get("/v1/products?limit=10")
+
+    @task(3)
+    def fetch_product(self):
+        self.client.get("/products?limit=10")
+
+    @task(1)
+    def get_home(self):
         self.client.get("/")
