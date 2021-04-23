c2639c18e01494790ba7e81fd85afc2b5fdf8cea
Nguyen Viet <vietnguyen148@gmail.com>
2021-04-24 06:54:57 +0700

    update migrate script

--- migrate.sh
+++ migrate.sh
@@ -2,8 +2,11 @@
 
 if [ $1 = "up" ]; then
 	echo "Upgrade version $1"
-	migrate -source file://./db/migrations/ -database 'mysql://root:123@tcp(localhost:3306)/tracking' up $2
+	./migrate -source file://./db/migrations/ -database 'mysql://root:123@tcp(localhost:3306)/tracking' up $2
 elif [ $1 = "down" ]; then
 	echo "Downgrade version $1"
-	migrate -source file://./db/migrations/ -database 'mysql://root:123@tcp(localhost:3306)/tracking' down $2
+	./migrate -source file://./db/migrations/ -database 'mysql://root:123@tcp(localhost:3306)/tracking' down $2
+elif [ $1 = "reset" ]; then
+	echo "Reset All"
+	./migrate -source file://./db/migrations/ -database 'mysql://root:123@tcp(localhost:3306)/tracking' drop -f
 fi
